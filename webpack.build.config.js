63d5ecb6970b468b99fb48426d2e4d706f1ff65f
PolGerdt <33346265+PolGerdt@users.noreply.github.com>
2019-06-25 06:41:03 +0200

    License...

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack')
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const BabiliPlugin = require('babili-webpack-plugin')
+//const BabiliPlugin = require('babili-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 // Any directories you will be adding code/files into, need to be added to this array so webpack will pick them up
